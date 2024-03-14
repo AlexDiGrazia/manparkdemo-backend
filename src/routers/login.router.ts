7838cdeddf68cbf49023a1ad1e9fde210cb15251
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-14 11:51:58 -0700

    include profile in login post request

--- src/routers/login.router.ts
+++ src/routers/login.router.ts
@@ -8,8 +8,11 @@ loginRouter.post("/", async (req, res) => {
   const username = req.body.username;
   const loggedInUser = await prisma.user.findUnique({
     where: { username },
+    include: {
+      profile: true,
+    },
   });
-  console.log(loggedInUser);
+  if (loggedInUser) console.log(loggedInUser.profile?.id);
   return res.status(201).send(loggedInUser);
 });
 
