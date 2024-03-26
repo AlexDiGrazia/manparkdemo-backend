110cdc30e033acec2e60e8e85b7f31c55a20c819
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-25 23:08:57 -0700

    added type TProfile

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,17 @@
+export type TProfile = {
+  bio: string;
+  birthday: Date | undefined;
+  home: string;
+  id: number;
+  occupation: string;
+  picture: string;
+  userId: number;
+  username: string;
+} | null; /* profile is an optional relationship in Prisma Schema */
+
 export type TUser = {
   id: number;
   username: string;
   password: string;
+  profile?: TProfile | undefined;
 };
