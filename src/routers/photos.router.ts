3516721899e001edce54ffd97eefd20bb980e777
Alex Digrazia <alexndigrazia@hotmail.com>
2024-03-28 23:16:24 -0700

    added photosRouter.post() so users can upload photo urls from Cloudinary to the database.   Also in this commit is an earlier Post method utiliziing Cloudinary's uploader.  I ended up not using it, for now, but wanted to keep it in the code base for reference

--- src/routers/photos.router.ts
+++ src/routers/photos.router.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { prisma } from "../../prisma/db.setup";
+import { v2 as cloudinary } from "cloudinary";
+import { validateRequest } from "zod-express-middleware";
+import { z } from "zod";
 
 const photosRouter = Router();
 
@@ -7,5 +10,39 @@ photosRouter.get("/", async (req, res) => {
   const allPhotos = await prisma.photo.findMany();
   res.status(200).send(allPhotos);
 });
+
+// photosRouter.post("/upload", async (req, res) => {
+//   console.log("end point connected");
+//   cloudinary.uploader.upload(
+//     "https://m.media-amazon.com/images/I/51hIdMnn1mS._AC_.jpg",
+//     { public_id: "cactus", folder: "Man Park Gallary" },
+
+//     function (error, result) {
+//       console.log(result);
+//       res.status(200).send(result);
+//     }
+//   );
+// });
+
+photosRouter.post(
+  "/upload",
+  validateRequest({
+    body: z.object({
+      image: z.string(),
+      date: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    const image = req.body.image;
+    const date = req.body.date;
+    const newPhoto = await prisma.photo.create({
+      data: {
+        image,
+        date,
+      },
+    });
+    res.status(200).send(newPhoto);
+  }
+);
 
 export { photosRouter };
